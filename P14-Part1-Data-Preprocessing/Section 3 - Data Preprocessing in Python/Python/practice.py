0d1997754efbca3e0a56386797a96b7fff2ce6f3
Havoccboi <havocisgod@gmail.com>
2021-09-25 17:15:19 +0600

    cleared

--- P14-Part1-Data-Preprocessing/Section 3 - Data Preprocessing in Python/Python/practice.py
+++ P14-Part1-Data-Preprocessing/Section 3 - Data Preprocessing in Python/Python/practice.py
@@ -1,29 +1,4 @@
 
-# -*- coding: utf-8 -*-
-"""
-Spyder Editor
-
-This is a temporary script file.
-"""
-
-#importing the libraries
-
-import pandas as pd
-import matplotlib.pyplot as plt
-import numpy as np
-
-
-#importing the dataset
-
-dataset = pd.read_csv('C:/Data.csv')
-X = dataset.iloc[:, :-1].values
-
-# -*- coding: utf-8 -*-
-"""
-Spyder Editor
-
-This is a temporary script file.
-"""
 
 #importing the libraries
 
