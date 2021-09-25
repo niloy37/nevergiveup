741a45eb439bd960eb150ceff06f2f8b2b9b5f29
Havoccboi <havocisgod@gmail.com>
2021-09-25 17:51:28 +0600

    categorial data processing

--- P14-Part1-Data-Preprocessing/Section 3 - Data Preprocessing in Python/Python/practice.py
+++ P14-Part1-Data-Preprocessing/Section 3 - Data Preprocessing in Python/Python/practice.py
@@ -19,3 +19,17 @@ from sklearn.impute import SimpleImputer
 imputer = SimpleImputer(missing_values = np.nan, strategy = 'mean')
 imputer = imputer.fit(X[:, 1:3])
 X[:, 1:3] = imputer.transform(X[:, 1:3])
+
+
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.compose import ColumnTransformer
+
+labelencoder_x = LabelEncoder()
+X[:,0]  = labelencoder_x.fit_transform(X[:,0])
+
+ct = ColumnTransformer([("Country", OneHotEncoder(), [0])], remainder = 'passthrough')
+X = ct.fit_transform(X)
+
+labelencoder_y  = LabelEncoder()
+Y = labelencoder_y.fit_transform(Y)
+
